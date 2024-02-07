1666dc6d0778f0db6410483aafda6302158a7518
Joel Soto <joelarlindosoto1@gmail.com>
2024-02-07 08:37:09 +0200

    fix: update answer not working fixed

--- src/main/java/com/Tech/quiz/Questions/service/impl/QuestionServiceImp.java
+++ src/main/java/com/Tech/quiz/Questions/service/impl/QuestionServiceImp.java
@@ -39,10 +39,23 @@ public class QuestionServiceImp implements QuestionService {
 
     public void updateQuestion(int questionId,Question question){
         Question questiontoUpdate = questionsRepository.findById(questionId).get();
+        List <Answer> answerstoUpdate =questiontoUpdate.getOptions();
+
+        for(byte i=0;i<3;i++){
+            Answer answerToUpdate=answerstoUpdate.get(i);
+            Answer currentAnswer= answerRepository.findById(answerToUpdate.getId()).get();
+            currentAnswer.setQuestion(questiontoUpdate);
+            currentAnswer.setName(answerToUpdate.getName());
+            currentAnswer.setCorrect(answerToUpdate.isCorrect());
+            answerstoUpdate.add(i,currentAnswer);
+        }
 
         questiontoUpdate.setQuestion(question.getQuestion());
-        questiontoUpdate.setOptions(question.getOptions());
+        questiontoUpdate.setOptions(answerstoUpdate);
         questionsRepository.save(questiontoUpdate);
+        answerRepository.saveAll(answerstoUpdate);
+
+
 
     }
 
